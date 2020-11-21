9c1a021dbacd5a6be5d42a255f06e4ad969f715e
vladislav <vladislavnikwork@gmail.com>
2020-11-21 16:54:49 +0200

    added snake eating logic

--- app/src/main/java/com/example/mysnake/MainActivity.kt
+++ app/src/main/java/com/example/mysnake/MainActivity.kt
@@ -15,13 +15,15 @@ const val HeadSize = 100
 
 class MainActivity : AppCompatActivity() {
 
+    val Feed by lazy { ImageView(this) }
+    val head by lazy { View(this)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val head = View(this)
-        head.layoutParams = LinearLayout.LayoutParams(HeadSize,HeadSize)
+        
+        head.layoutParams = FrameLayout.LayoutParams(HeadSize,HeadSize)
         head.background = ContextCompat.getDrawable( this, R.drawable.circle)
         container.addView(head)
         Start();
@@ -29,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         icArrowUp.setOnClickListener { Snake.SnakeMove = {move(Directions.Up, head)} }
         icArrowDown.setOnClickListener { Snake.SnakeMove = {move(Directions.Down, head)} }
         icArrowLeft.setOnClickListener { Snake.SnakeMove = {move(Directions.Left, head)} }
-        icArrowRight.setOnClickListener { Snake.SnakeMove = {move(Directions.Right, head)} }
+        icArrowRight.setOnClickListener { Snake.SnakeMove = {move(Directions.Right,head)} }
         icPause.setOnClickListener { isPlay=false }
         icPlay.setOnClickListener { isPlay=true }
     }
@@ -42,23 +44,36 @@ class MainActivity : AppCompatActivity() {
             Directions.Right -> (head.layoutParams as FrameLayout.LayoutParams).leftMargin +=HeadSize
         }
         runOnUiThread {
-            GenerateFeed()
+            EatSnake(head)
             container.removeView(head)
             container.addView(head)
         }
     }
 
-    private fun GenerateFeed(){
+    private fun GenerateFeed(head: View){
         Thread(Runnable {
-            val Feed = ImageView(this)
-            Feed.layoutParams = FrameLayout.LayoutParams(HeadSize,HeadSize)
-            Feed.setImageResource(R.drawable.ic_feed)
-            (Feed.layoutParams as FrameLayout.LayoutParams).topMargin = (1..10).random() * HeadSize
-            (Feed.layoutParams as FrameLayout.LayoutParams).leftMargin = (1..10).random() * HeadSize
+            var generate=true
+            while(generate) {
+                Feed.layoutParams = FrameLayout.LayoutParams(HeadSize,HeadSize)
+                Feed.background = ContextCompat.getDrawable( this, R.drawable.ic_feed)
+                (Feed.layoutParams as FrameLayout.LayoutParams).topMargin = (1..10).random() * HeadSize
+                (Feed.layoutParams as FrameLayout.LayoutParams).leftMargin = (1..10).random() * HeadSize
+                if (head.top != Feed.top || head.left != Feed.left){
+                    generate = false
+                }
+            }
             runOnUiThread{
                 container.addView(Feed)
             }
         }).start()
+    }
+
+    private fun EatSnake(head: View){
+        if(head.top==Feed.top && head.left==Feed.left)
+        {
+            container.removeView(Feed)
+            GenerateFeed(head)
+        }
     }
 }
 
